b75c901e9f47f7163b503dfaac45c36f1f412876
Edgeburn <edgeburn02@mailredirect.edgeburnmedia.com>
2023-04-22 23:43:31 -0400

    first commit

---
+++ src/main/java/com/edgeburnmedia/incantare/incantare/Incantare.java
@@ -0,0 +1,67 @@
+package com.edgeburnmedia.incantare.incantare;
+
+import com.edgeburnmedia.incantare.incantare.enchantment.ShatterproofEnchantment;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Incantare.MODID)
+public class Incantare {
+
+	// Define mod id in a common place for everything to reference
+	public static final String MODID = "incantare";
+	// Directly reference a slf4j logger
+	private static final Logger LOGGER = LogUtils.getLogger();
+	// Create a Deferred Register to hold Blocks which will all be registered under the "Incantare" namespace
+	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(
+		ForgeRegistries.BLOCKS, MODID);
+	// Create a Deferred Register to hold Items which will all be registered under the "Incantare" namespace
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
+		ForgeRegistries.ITEMS, MODID);
+
+	public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(
+		ForgeRegistries.ENCHANTMENTS, MODID);
+
+	public static final RegistryObject<Enchantment> SHATTERPROOF_ENCHANTMENT = ENCHANTMENTS.register("shatterproof",
+		ShatterproofEnchantment::new);
+
+	public Incantare() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		// Register the commonSetup method for modloading
+		modEventBus.addListener(this::commonSetup);
+
+		ENCHANTMENTS.register(modEventBus);
+
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void commonSetup(final FMLCommonSetupEvent event) {
+
+		// Some common setup code
+		LOGGER.info("HELLO FROM COMMON SETUP");
+		LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+	}
+
+	// You can use SubscribeEvent and let the Event Bus discover methods to call
+	@SubscribeEvent
+	public void onServerStarting(ServerStartingEvent event) {
+		// Do something when the server starts
+		LOGGER.info("HELLO from server starting");
+	}
+}
---
+++ src/main/java/com/edgeburnmedia/incantare/incantare/enchantment/ShatterproofEnchantment.java
@@ -0,0 +1,130 @@
+package com.edgeburnmedia.incantare.incantare.enchantment;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.ShieldBlockEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ShatterproofEnchantment extends Enchantment {
+
+	public ShatterproofEnchantment() {
+		super(Rarity.RARE, EnchantmentCategory.BREAKABLE,
+			new EquipmentSlot[]{EquipmentSlot.MAINHAND, EquipmentSlot.HEAD, EquipmentSlot.CHEST,
+				EquipmentSlot.LEGS, EquipmentSlot.FEET});
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public void onItemUse(LivingEntityUseItemEvent.Start event) {
+		if (!event.getEntity().getLevel().isClientSide()) {
+			ItemStack stack = event.getItem();
+			if (stack != null) {
+				event.setCanceled(protectItem(stack));
+			}
+
+		}
+	}
+
+	@SubscribeEvent
+	public void onBlockDamage(PlayerEvent.BreakSpeed event) {
+		ItemStack stack = event.getEntity().getMainHandItem();
+		if (stack != null) {
+			if (protectItem(stack)) {
+				event.setCanceled(true);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onArmourEquip(LivingEquipmentChangeEvent event) {
+		ItemStack stack = event.getTo();
+		if (stack != null) {
+			if (protectItem(stack)) {
+				event.setCanceled(true);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityHurt(LivingHurtEvent event) {
+		if (!event.getEntity().getLevel().isClientSide()) {
+			Entity entity = event.getEntity();
+			for (ItemStack armorSlot : entity.getArmorSlots()) {
+				if (protectItem(armorSlot)) {
+					if (entity instanceof ServerPlayer player) {
+						if (!player.getInventory().add(armorSlot)) {
+							ItemEntity itemEntity = new ItemEntity(player.level, player.getX(),
+								player.getY(), player.getZ(), armorSlot);
+							player.level.addFreshEntity(itemEntity);
+							player.getInventory().removeItem(armorSlot);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onAttackEntity(AttackEntityEvent event) {
+		if (!event.getEntity().getLevel().isClientSide()) {
+			ItemStack stack = event.getEntity().getMainHandItem();
+			if (stack != null) {
+				if (protectItem(stack)) {
+					event.setCanceled(true);
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onShieldDefend(ShieldBlockEvent event) {
+		if (!event.getEntity().getLevel().isClientSide()) {
+			ItemStack stack = event.getEntity().getUseItem();
+			if (stack != null) {
+				if (protectItem(stack)) {
+					event.setShieldTakesDamage(false);
+					event.setCanceled(true);
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void renderTooltip(ItemTooltipEvent event) {
+		ItemStack stack = event.getItemStack();
+		if (stack != null) {
+			int level = stack.getEnchantmentLevel(this);
+			if (level > 0 && stack.getMaxDamage() - stack.getDamageValue() <= 2) {
+				event.getToolTip().add(
+					Component.literal("Broken").withStyle(ChatFormatting.RED, ChatFormatting.BOLD));
+			}
+		}
+	}
+
+	protected boolean protectItem(ItemStack stack) {
+		int level = stack.getEnchantmentLevel(this);
+		if (level > 0) {
+			int durabilityRemaining = stack.getMaxDamage() - stack.getDamageValue();
+			if (durabilityRemaining <= 2) {
+				stack.setDamageValue(Math.abs(2 - stack.getMaxDamage()));
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
